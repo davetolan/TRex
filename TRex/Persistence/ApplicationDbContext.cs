d3de451e98bdf23cff59e18186ea2d2ababfd87a
David Tolan <tolan.david@gmail.com>
2018-05-04 22:25:38 -0500

    Split Identity Models file into separate files for classes

---
+++ TRex/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using TRex.Models;
+
+namespace TRex.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
