d057e64ad96e87bba74316dd1a4e0afec5c00160
David Tolan <tolan.david@gmail.com>
2018-05-04 22:13:20 -0500

    Add project files.

---
+++ TRex/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TRex.Startup))]
+namespace TRex
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
